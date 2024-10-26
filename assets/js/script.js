eb0da397fe4c757a1b7970e266bc8960e5a8a56e
ehsan sayyadi <ehsansayadi7189@gamil.com>
2024-10-27 02:13:37 +0330

    0

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+const data = [
+    {
+        title: 'Fjallraven - Foldsack No…ckpack, Fits 15 Laptops',
+        image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+        desc: 'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday'
+    },
+    {
+        title: 'Fjallraven - Foldsack No…ckpack, Fits 15 Laptops',
+        image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+        desc: 'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday'
+    },
+    {
+        title: 'Fjallraven - Foldsack No…ckpack, Fits 15 Laptops',
+        image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+        desc: 'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday'
+    },
+    {
+        title: 'Fjallraven - Foldsack No…ckpack, Fits 15 Laptops',
+        image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+        desc: 'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday'
+    },
+    {
+        title: 'Fjallraven - Foldsack No…ckpack, Fits 15 Laptops',
+        image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+        desc: 'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday'
+    },
+
+]
+const row = document.getElementById('product-container');
+data.map(item => {
+    const col = document.createElement('div');
+    col.classList.add('col-lg-3', 'col-sm-12');
+    col.innerHTML = `
+    <div class="card" style="width: 18rem;">
+  <img src="${item.image}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${item.title}</h5>
+    <p class="card-text">${item.desc}</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>`
+})
